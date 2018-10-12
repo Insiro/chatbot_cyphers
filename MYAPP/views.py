cc0f14b7c104c2543b2bc38686842e0a314111e6
Insiro <insiro@naver.com>
2018-10-12 20:22:42 +0900

    Update views.py

--- MYAPP/views.py
+++ MYAPP/views.py
@@ -1,3 +1,15 @@
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+import json
+ 
+ 
+ 
+def keyboard(request):
+ 
+    return JsonResponse({
+        'type':'buttons',
+        'buttons':['오늘','내일']
     })
  
 @csrf_exempt
