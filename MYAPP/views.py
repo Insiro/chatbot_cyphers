6b986f96711507e017757fa5eab862c8c736644e
Insiro <insiro@naver.com>
2018-11-15 01:30:08 +0900

    Update views.py

--- MYAPP/views.py
+++ MYAPP/views.py
@@ -5,8 +5,7 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
-
-defualtList = ['랭킹','전적검색','오싸']
+defualtList = ['랭킹', '전적검색', '오싸']
 
 
 def keyboard(request):
@@ -14,6 +13,8 @@ def keyboard(request):
         'type': 'buttons',
         'buttons': defualtList
     })
+
+
 status = 0
 season = 13
 charactor = {'로라스': 'loras', '휴톤': 'huton', '루이스': 'louis', '타라': 'tara', '트리비아': 'trivia', '카인': 'cain',
@@ -25,7 +26,8 @@ charactor = {'로라스': 'loras', '휴톤': 'huton', '루이스': 'louis', '타
              '엘리': 'ellie', '마틴': 'martin', '브로스': 'bruce', '미아': 'mia', '드니스': 'denise', '제레온': 'gereon', '루시': 'lucy',
              '티엔': 'tian', '하랑': 'harang', 'J': 'j', 'j': 'j', '제이': 'j', '벨져': 'belzer', '리첼': 'richel', '리사': 'risa',
              '릭': 'rick', '제키엘': 'jekiel', '탄야': 'tanya', '캐럴': 'carol', '라이샌더': 'lysander', '루드빅': 'ludwig',
-             '멜빈': 'melvin', '디아나': 'diana', '클리브': 'clive', '헬레나': 'helena', '에바': 'eva', '론': 'ron', '레오노르': 'leonor', '시드니': 'sidney'
+             '멜빈': 'melvin', '디아나': 'diana', '클리브': 'clive', '헬레나': 'helena', '에바': 'eva', '론': 'ron', '레오노르': 'leonor',
+             '시드니': 'sidney'
              }
 
 
@@ -96,7 +98,7 @@ def charRank(data):
         status = 0
     else:
         return textRespone('존재하지 않는 이름입니다.\n올바른 캐릭터명을 입력하시오')
-    link='http://cyphers.nexon.com/cyphers/article/ranking/charac/13/' + charactor.get(data) + '/win/day/1'
+    link = 'http://cyphers.nexon.com/cyphers/article/ranking/charac/13/' + charactor.get(data) + '/win/day/1'
     req = requests.get(link)
     html = req.text
     soup = BeautifulSoup(html, 'html.parser')
@@ -109,9 +111,9 @@ def charRank(data):
             i += 1
             if j != 0:
                 str2 += '\n'
-            str2 += str(j+1)
+            str2 += str(j + 1)
         elif i == 1:
-            str2 += ' ' + tag.text[:7]+' '
+            str2 += ' ' + tag.text[:7] + ' '
             i += 1
         elif i == 4:
             i = 0
@@ -123,6 +125,7 @@ def charRank(data):
     print(str2)
     contents = str2
     return btnRespone(contents, defualtList)
+
 
 def parseHistory(data):
     link = 'http://cyphers.nexon.com/cyphers/game/log/search/1/' + data
@@ -146,9 +149,10 @@ def parseHistory(data):
         strs[i] += " " + "%-4s" % stsr[1] + "\t" + stsr[3][0:2]
         j += 1
         i += 1
-    if j!=0:
+    if j != 0:
         con = "\n".join(strs)
     return con
+
 
 def parseFight():
     link = 'http://cyphers.nexon.com/cyphers/article/ranking/gof/f/1'
@@ -174,13 +178,13 @@ def parseFight():
         elif i == 4:
             i = 0
             j += 1
-            str2 += ''.join(tag.text[5:].split(' '))
+            str2 += ''.join(tag.text[4:].split(' '))
         else:
             i += 1
             continue
     return str2
-    
-    
+
+
 def parseRank():
     link = 'http://cyphers.nexon.com/cyphers/article/ranking/total/13/1'
     req = requests.get(link)
